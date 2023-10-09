af5666dfa67d661909916c52eba8eff849f1e0c2
Maicon Silveira <maiconsb2041@gmail.com>
2023-10-09 19:19:59 -0300

    Product

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -1,13 +1,16 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 
 @Entity
@@ -20,10 +23,13 @@ public class Category implements Serializable{
 	private Long id;
 	private String name;
 	
+	@Transient
+	private Set<Product> products = new HashSet<>();
+	
 	public Category() {}
 
 	public Category(Long id, String name) {
-		super();
+		
 		this.id = id;
 		this.name = name;
 	}
@@ -39,6 +45,10 @@ public class Category implements Serializable{
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
+	}
+	
+	public Set<Product> getListProducts() {
+		return products;
 	}
 
 	@Override
